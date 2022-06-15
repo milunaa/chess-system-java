6bfa8692ea668d276a058025e1e5171e143ac31c
milunaa <milenalunadosantos@gmail.com>
2022-06-15 11:21:18 -0300

    Queen

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -32,7 +32,7 @@ public class Bishop extends ChessPiece {
 	    	mat[p.getRow()][p.getColumn()] = true;
 	    }
 	    
-	 // ne
+	    // ne
 	    p.setValues(position.getRow() - 1, position.getColumn() + 1);
 	    while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 	        mat[p.getRow()][p.getColumn()] = true;
@@ -42,7 +42,7 @@ public class Bishop extends ChessPiece {
 	    	mat[p.getRow()][p.getColumn()] = true;
 	    }
 	    
-	 // se
+	    // se
 	    p.setValues(position.getRow() + 1, position.getColumn() + 1);
 	    while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 	        mat[p.getRow()][p.getColumn()] = true;
@@ -52,7 +52,7 @@ public class Bishop extends ChessPiece {
 	    	mat[p.getRow()][p.getColumn()] = true;
 	    }
 	    
-	 // sw
+	    // sw
 	    p.setValues(position.getRow() + 1, position.getColumn() - 1);
 	    while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 	        mat[p.getRow()][p.getColumn()] = true;
